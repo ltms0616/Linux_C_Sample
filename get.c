dd80761a81e30366218f2937b0234045748de68e
Moore Lin <ltms0616@gmail.com>
2018-03-20 18:28:59 +0800

    add get.c include usage of fgets,fgetc,fseek,ftell,rewind

---
+++ get.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* fgets(char *s, int size, FILE *stream) 
+   int fseek(FILE *stream, long offset, int whence)   whence= SEEK_SET ==> offset is related to start of file
+                                                          SEEK_CUR ==> offset is related to current position
+                                                          SEEK_END ==> offset is related to end of file
+   long ftell(FILE *stream)  currnet position
+   void rewind(FILE *stream) set position indicator to the begin of the file
+   int fgetc(FILE *stream) read next character from stream, until EOF, return as unsigned char cast to int
+*/
+
+void main(int argc, char** argv)
+{
+    FILE *pfile;
+    char string[100];
+    int rc=0;
+   
+    pfile = fopen("poem.txt", "r");
+
+    if(pfile == NULL){
+        printf("open file fail!");
+    }
+
+    printf("fgets example\n");
+    if(fgets(string, 200, pfile)){
+	puts(string);
+    }else{
+	printf("fgets fail!\n");
+    }
+
+    /*rc = fseek(pfile, 0, SEEK_SET);*/
+    rewind(pfile);
+    printf("current file pointer position = %ld\n", ftell(pfile));  
+    printf("fgetc example\n");
+    
+    rc = fseek(pfile, 15, SEEK_SET);
+    rc = fgetc(pfile);
+    printf("%d, %c\n", rc, rc);
+
+    return;
+}
