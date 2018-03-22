2f4c601b0677b3d56f3fe273e50df6612fe2e602
Moore Lin <ltms0616@gmail.com>
2018-03-22 18:37:33 +0800

    add scan.c

--- get.c
+++ get.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
-/* fgets(char *s, int size, FILE *stream) 
+/* char fgets(char *s, int size, FILE *stream) 
    int fseek(FILE *stream, long offset, int whence)   whence= SEEK_SET ==> offset is related to start of file
-                                                          SEEK_CUR ==> offset is related to current position
+								  SEEK_CUR ==> offset is related to current position
                                                           SEEK_END ==> offset is related to end of file
    long ftell(FILE *stream)  currnet position
    void rewind(FILE *stream) set position indicator to the begin of the file
    int fgetc(FILE *stream) read next character from stream, until EOF, return as unsigned char cast to int
+
+   getchar
+   getc
+   gets
 */
 
 void main(int argc, char** argv)
@@ -16,7 +20,7 @@ void main(int argc, char** argv)
     FILE *pfile;
     char string[100];
     int rc=0;
-   
+#if 0   
     pfile = fopen("poem.txt", "r");
 
     if(pfile == NULL){
@@ -29,6 +33,10 @@ void main(int argc, char** argv)
     }else{
 	printf("fgets fail!\n");
     }
+    
+    printf("fgets from stdin\n");
+    fgets(string, 5, stdin);
+    printf("the input is %s\n", string);
 
     /*rc = fseek(pfile, 0, SEEK_SET);*/
     rewind(pfile);
@@ -38,6 +46,8 @@ void main(int argc, char** argv)
     rc = fseek(pfile, 15, SEEK_SET);
     rc = fgetc(pfile);
     printf("%d, %c\n", rc, rc);
+#endif
 
+    
     return;
 }
---
+++ scan.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void main(int argc, char** argv)
+{
+/*
+    int scanf(const char *format, ...)  ==> read input from stdin according to format conversion
+    int sscanf(const char *str, const char* format, ...)  ==> read input from char string pointed by str
+    int fscanf(FILE *stream, const char* format, ...)  ==> read input from the point stream
+*/
+
+    int rc=0;
+    char *p;
+    char *p1;    
+    int len=20;
+    FILE *pfile;
+    char text[20];    
+
+    p = malloc(len*sizeof(char));
+    p1 = malloc(len*sizeof(char));
+#if 0
+    printf("*****scanf input*****\n");
+    printf("scan wihtout space\n");    
+    rc = scanf("%s", p);
+    printf("the input is %s\n", p);
+    memset(p, 0, len*sizeof(char));
+    printf("scan two string with space\n");
+    rc = scanf("%s %s", p, p1);
+    printf("the input is %s %s\n", p, p1);
+
+    /* Note scanf and fscanf is the same when input is stdin, fscan can also input from other stream*/
+    printf("*****fscanf input*****\n");
+    memset(p, 0, len*sizeof(char));
+    rc = fscanf(stdin, "%s", p);
+    printf("the input is %s\n", p);
+#endif
+    pfile = fopen("poem.txt", "r");
+    
+    fgets(p , 20, pfile);
+    printf("%s", p);
+    rc = sscanf(p, "%s", p1);
+    printf("%s\n", p1);    
+
+
+          
+    free(p1);
+    free(p);
+
+
+}
+
+
